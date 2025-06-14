9c97383c92f373bdbc41bf70130c189868161207
Pol Kaulfield <kaulfield@protonmail.com>
2025-06-14 20:03:16 +0200

    Disabled screen locking for live session

---
+++ scripts/debs/calamares-settings-privos/usr/bin/disable-kde-lock.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Disable KDE autolock
+kwriteconfig5 --file kscreenlockerrc --group Daemon --key Autolock false
+kwriteconfig5 --file kscreenlockerrc --group Daemon --key LockOnResume false
+
+# Disable power management idle time
+kwriteconfig5 --file powermanagementprofilesrc --group AC --key idleTime 0
+kwriteconfig5 --file powermanagementprofilesrc --group AC --key lidAction 0
+kwriteconfig5 --file powermanagementprofilesrc --group AC --key suspendWhenIdle false
+kwriteconfig5 --file powermanagementprofilesrc --group AC --key suspendThenHibernate false
+
+# Reapply power management config
+qdbus org.kde.Solid.PowerManagement /org/kde/Solid/PowerManagement reparseConfiguration
